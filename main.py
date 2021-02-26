bcefafa11c2095546afb6759d9fdac5ebc75d855
Henry Cheng <chengh3@petinsurance.com>
2021-02-26 11:59:37 -0800

    add command line argument

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 '''
-This module move file from local project folder to shared drive
+Date    02/26/21
+Author  Henry Cheng
+Status  Dev
+
+This module move file from local project folder to shared drive.
+Purpose is to be ran every Friday to upload any local work to shared drive as a backup.
 '''
+import os
+import sys
+
+path_source = sys.argv[1]
+path_destination = sys.argv[2]
+
+os.replace()
