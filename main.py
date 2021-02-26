9cbd483eb681dd2a16d418f78350fe5b67ecf858
Henry Cheng <chengh3@petinsurance.com>
2021-02-26 12:36:55 -0800

    add feature: move file to destination folder

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ Status  Dev
 This module move file from local project folder to shared drive.
 Purpose is to be ran every Friday to upload any local work to shared drive as a backup.
 '''
-import os
-import sys
+import sys, os, shutil, glob
 
 path_source = sys.argv[1]
 path_destination = sys.argv[2]
 
-os.replace()
+print(os.listdir(path_source))
+print(os.path.join(path_source, '*.*'))
+print(glob.glob(path_source))
+
+for file in glob.glob(path_source):
+    shutil.copy2(file, path_destination)
